1b3b5961275314901e9c1b35db3ede153af0781c
keenonkites <keenonkites@gmx.net>
2013-12-25 22:34:06 +0100

    Combinded the --check-styles and the normal compile of the map into the same command.

--- Freizeitkarte-Entwicklung/mt.pl
+++ Freizeitkarte-Entwicklung/mt.pl
@@ -1930,12 +1930,8 @@ sub build_map {
   # Lizenz-File kopieren
   copy ( "$BASEPATH/license.txt", "license.txt" ) or die ( "copy() failed: $!\n" );
 
-  # Run the check-style first to make sure there are no problems inside the style files...
+  # OSM-Daten compilieren (-Dlog.config=logging.properties) (with checking style files first with --check-styles)
   $command = "java -Xmx" . $javaheapsize . "M" . " -jar $BASEPATH/tools/mkgmap/mkgmap.jar $max_jobs -c $mapname.cfg --check-styles";
-  process_command ( $command );
-
-  # OSM-Daten compilieren (-Dlog.config=logging.properties)
-  $command = "java -Xmx" . $javaheapsize . "M" . " -jar $BASEPATH/tools/mkgmap/mkgmap.jar $max_jobs -c $mapname.cfg";
   process_command ( $command );
 
   return;
